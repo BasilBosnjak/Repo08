4948068522ae1e1e314edff9626670e93eddfbb9
BasilBosnjak <basil.bosnjak@gmail.com>
2021-09-21 01:26:06 +0200

    Adding exercises 13 to 16

---
+++ ex14/src/index.js
@@ -0,0 +1,9 @@
+function convertStrToInt(str) {
+    return parseInt(str);
+}
+
+console.log (parseInt("44"));
+console.log (parseInt("27"));
+console.log (parseInt("JeffBezos"));
+
+module.exports = convertStrToInt;
